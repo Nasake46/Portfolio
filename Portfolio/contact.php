7133be91aea8271b5f2a5602c713f353e998007c
Nasake46 <wilfrid.salles-otta@supinternet.fr>
2021-04-14 21:56:12 +0200

    Update

---
+++ Portfolio/contact.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	
+	<title>Contact</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+	<script src="https://kit.fontawesome.com/8aaac84553.js" crossorigin="anonymous"></script>
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li><a href="index.php">Accueil</a></li>
+					<li><a href="about.php">A propos</a></li>
+					<li class="active"><a href="contact.php">Contact</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+
+	<header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb">
+			<li><a href="index.php">Accueil</a></li>
+			<li class="active">A propos</li>
+		</ol>
+
+		<div class="row">
+			
+			<!-- Article main content -->
+			<article class="col-sm-9 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">Contactez moi</h1>
+				</header>
+				
+				<p>
+					Pour prendre contatc avec moi, n'hésitez pas à mettre vos informations ci-dessous, ce sera avec plaisir que je vous répondrais !
+				</p>
+				<br>
+					<form>
+						<div class="row">
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Nom">
+							</div>
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Email">
+							</div>
+							<div class="col-sm-4">
+								<input class="form-control" type="text" placeholder="Numéro de téléphone">
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-12">
+								<textarea placeholder="Tapez votre message ici..." class="form-control" rows="9"></textarea>
+							</div>
+						</div>
+						<br>
+						<div class="row">
+							<div class="col-sm-6 text-right">
+								<input class="btn btn-action" type="submit" value="Send message">
+							</div>
+						</div>
+					</form>
+
+			</article>
+			<!-- /Article -->
+
+		</div>
+	</div>	<!-- /container -->
+	
+	<div class="container-full top-space">
+</div>
+
+	<footer id="footer">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Contact</h3>
+						<div class="widget-body">
+							<p>07 52 39 57 39<br>
+								<a href="mailto:#">wilfrid.salles-otta@supinternet.fr</a><br>
+								<br>
+								Paris, Île de France
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Suivez moi</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons clearfix">
+							<a href="https://github.com/Nasake46"><i class="fa fa-github fa-2"></i></a>
+							<a href="https://www.linkedin.com/in/wilfrid-salles-otta-84b464207/"><i class="fab fa-linkedin"></i></a>
+							<a href="https://twitter.com/OttaSalles"><i class="fab fa-twitter"></i></a>
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">A propos</h3>
+						<div class="widget-body">
+						<p>Etudiant developpement à Epitech Digital</p>
+							<p>Maîtrise du JavaScript</p>
+							<p>Maîtrise du Html et du css</p>
+							<p>Maîtrise du Php et des bases de données avec PhpMyAdmin</p>
+							<p>1ère année d'Etude</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="simplenav">
+								<a href="#">Accueil</a> | 
+								<a href="about.php">A propos</a> |
+								<a href="contact.php">Contact</a> |
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="text-right">
+								Copyright &copy; 2021, Salles Otta 
+							</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+	</footer>	
+		
+
+
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	
+
+</body>
+</html>
---
+++ Portfolio/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Portfolio assez important qui vous montrera ce que je fais">
+	
+	<title>Portfolio</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+	<script src="https://kit.fontawesome.com/8aaac84553.js" crossorigin="anonymous"></script>
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body class="home">
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">Accueil</a></li>
+					<li><a href="about.php">A propos</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<img src="assets/images/Class.jpg" class="image" alt="">
+				<h1 class="lead">Wilfrid Salles Otta</h1>
+				<p class="tagline">Web Developper d'Epietch Digital</p>
+				<p><a class="btn btn-default btn-lg" role="button" href="about.php">Plus d'infos</a></p>
+			</div>
+		</div>
+	</header>
+		<div class="container">
+			
+			<h3 class="text-center thin">Exemple de mes projets</h3>
+			
+			<div class="row">
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fas fa-video"></i>Tutube</h4></div>
+					<div class="h-body text-center">
+						<p>Un Youtubeur amateur où il est possible de regarder des vidéos et se connecter afin de pouvoir en ajouter, en supprimer ou bien modifier leurs descriptions </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fas fa-city"></i>Base de donnée d'une ville</h4></div>
+					<div class="h-body text-center">
+						<p>Format théorique. Base de donnée contenant chaque habitant avec leur métier et numéro d'habitation. En fonction de leur métier, l'endroit où ils travaillent est stocké, ainsi que les ingrédients utilisés dans des magasins, et enfin un système de vote opérationnel </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="far fa-window-maximize"></i>Site de streaming</h4></div>
+					<div class="h-body text-center">
+						<p>Site pour les artistes en tout genre ayant du mal à organiser leurs concert à cause de la pandémie mondiale. Possibilité de les rémunérer, de voir des concerts en live ainsi que meurs rediffusions. Pour finir, possibilité de voir en vr et pour les artistes, de lancer leurs lives en se connectant</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fas fa-window-maximize"></i>Site de télétravail</h4></div>
+					<div class="h-body text-center">
+						<p>Site ayant pour but d'aider un domaine particulier et d'optimiser le télétravail via un site qui enlèverait toutes les problématique de ce problème</p>
+					</div>
+				</div>
+			</div> <!-- /row  -->
+		
+		</div>
+	<!-- /Highlights -->
+
+	<!-- /social links -->
+
+
+	<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Contact</h3>
+						<div class="widget-body">
+							<p>07 52 39 57 39<br>
+								<a href="#">wilfrid.salles-otta@supinternet.fr</a><br>
+								<br>
+								Paris, Île de France
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Suivez moi</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons">
+							<a href="https://github.com/Nasake46"><i class="fa fa-github fa-2"></i></a>
+							<a href="https://www.linkedin.com/in/wilfrid-salles-otta-84b464207/"><i class="fab fa-linkedin"></i></a>
+							<a href="https://twitter.com/OttaSalles"><i class="fab fa-twitter"></i></a>
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">A propos</h3>
+						<div class="widget-body">
+							<p>Etudiant developpement à Epitech Digital</p>
+							<p>Maîtrise du JavaScript</p>
+							<p>Maîtrise du Html et du css</p>
+							<p>Maîtrise du Php et des bases de données avec PhpMyAdmin</p>
+							<p>1ère année d'Etude</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="simplenav">
+								<a href="#">Accueil</a> | 
+								<a href="about.php">A propos</a> |
+								<a href="contact.php">Contact</a> |
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="text-right">
+								Copyright &copy; 2021, Salles Otta Wilfrid.
+							</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+	</footer>	
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+</body>
+</html>
